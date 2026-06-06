56456b189da152010cb1c600abf751a11401741f
NalenSrin123 <srinnalen@gmail.com>
2026-06-07 06:54:39 +0700

    fix not declare inventory list

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import ResetPassword from '@/pages/auth/ResetPassword.vue'
 import Forgot_Password from '@/pages/auth/Forgot_Password.vue'
 import FromInventory from '@/pages/dashboard/FromInventory.vue'
 import Verify_otp from '@/pages/auth/Verify_OTP.vue'
+import InventoryList from '@/pages/dashboard/InventoryList.vue'
 /**
  * Route definitions
  * Each route maps a URL path to a specific page component
