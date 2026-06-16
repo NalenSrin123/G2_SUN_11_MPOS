66f620924fdb344d91607688a51c1aab38994083
GiO234765 <brokaknj@gmail.com>
2026-06-16 11:18:21 +0700

    design_main_product

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,7 @@ import Register from '@/pages/auth/Register.vue'
 import Preview from '@/pages/preview/Preview.vue'
 import Home from '@/pages/home/Home.vue'
 import NotFound from '@/pages/NotFound.vue'
+import MainMenu from '@/pages/home/main_menu.vue'
 
 /**
  * Route definitions
@@ -55,6 +56,7 @@ const routes = [
    */
   { path: '/login', component: Login },
   { path: '/register', component: Register },
+  { path: '/main_menu', component: MainMenu },
 
   /**
    * Optional home route (disabled for now)
