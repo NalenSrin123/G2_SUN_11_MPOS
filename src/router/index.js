aaed9e741a9f1a97be831c9ccec6a1542e7d16a3
NalenSrin123 <srinnalen@gmail.com>
2026-07-05 12:30:07 +0700

    Import component

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,7 @@ import MainMenu from "@/pages/home/main_menu.vue";
 import Cart from "@/pages/home/Design_listcartpage.vue";
 import Scan_To_pay from "../pages/home/home/Scan_To_pay.vue";
 import Product_Detail from "../pages/home/Product_Detail.vue";
+import Update_Product from "../pages/dashboard/Update_Product.vue";
 function isAuthenticated() {
   return !!localStorage.getItem("auth_token");
 }
